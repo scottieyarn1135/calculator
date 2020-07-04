a0089597b6f2d8c6f639f32a25960ce0b50a09ad
SA <62649689+scottyarn@users.noreply.github.com>
2020-07-04 19:52:43 +0100

    Add files via upload

--- calcgang2.0.py
+++ calcgang2.0.py
@@ -1,9 +1,6 @@
 print("Easy 2.0 calcgang")
-
-print("Welcome to the calc EU 2020")
-
+# Mode
 print("Select a mode")
-
 print('''
         Mode 1 - Add
         Mode 2 - Subtract
@@ -11,18 +8,35 @@ print('''
         Mode 4 - Power
         Mode 5 - Divide
                        ''')
-
+# User Input
 Mode = int(input("Select a Mode!: "))
+N1 = float(input("First Number here!: "))
+N2 = float(input("Second Number here!: "))
+# Funtions
+def Add():
+    return N1 + N2
+
+def Subtract():
+    return N1 - N2
+    
+def Mutiple():
+    return N1 * N2
+
+def power():
+    return N1 ** N2
+
+def Divide():
+    return N1 / N2 
+# When user picks a mode.
 if Mode == 1:
-    pass
+    print(f"{N1} + {N2} = {Add()}")
 elif Mode == 2:
-    pass
+    print(f"{N1} - {N2} = {Subtract()}") 
 elif Mode == 3:
-    pass
+    print(f"{N1} + {N2} = {Mutiple()}")
 elif Mode == 4:
-    pass
+    print(f"{N1} * {N2} = {power()}")
 elif Mode == 5:
-    pass
+    print(f"{N1} / {N2} = {Divide()}")
 else:
-    print("What are you doing in my swamp") 
-   
+    print("What are you doing in my swamp")
