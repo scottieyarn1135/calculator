dcc93f1fcab93f9b6c7f0a032c2b16f5a2877513
SA <62649689+scottyarn@users.noreply.github.com>
2020-07-04 18:53:20 +0100

    Add files via upload

---
+++ calcgang2.0.py
@@ -0,0 +1,28 @@
+print("Easy 2.0 calcgang")
+
+print("Welcome to the calc EU 2020")
+
+print("Select a mode")
+
+print('''
+        Mode 1 - Add
+        Mode 2 - Subtract
+        Mode 3 - Mutiply
+        Mode 4 - Power
+        Mode 5 - Divide
+                       ''')
+
+Mode = int(input("Select a Mode!: "))
+if Mode == 1:
+    pass
+elif Mode == 2:
+    pass
+elif Mode == 3:
+    pass
+elif Mode == 4:
+    pass
+elif Mode == 5:
+    pass
+else:
+    print("What are you doing in my swamp") 
+   
